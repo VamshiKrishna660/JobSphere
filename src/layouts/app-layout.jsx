a298cc55c397749bab8a2bf44bffc13ac99e4100
Vamshi Krishna <146640385+VamshiKrishna660@users.noreply.github.com>
2025-02-17 16:33:39 +0530

    Deployment

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,25 @@
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container px-8 ">
+        <Header />
+        <Outlet />
+      </main>
+      
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        <p className="text-sm text-gray-400">
+          &copy; {new Date().getFullYear()} JobSphere. All rights reserved.
+        </p>
+        <p className="text-sm text-gray-400">
+          Designed and developed with care to provide exceptional user experience and innovative solutions.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
